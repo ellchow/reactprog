0352bbc27ca9617d55f11b2199ce0a279fef16d5
ellchow <ellchow@ebay.com>
2013-11-04 20:47:13 -0800

    property checks for heap

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -14,7 +14,25 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
-  lazy val genHeap: Gen[H] = ???
+  property("") = forAll{  (as: List[A]) =>
+    val h = insertAll(empty, as: _*)
+    val decr = readAllDecreasing(h)
+
+    decr == as.sorted.reverse
+  }
+
+  def insertAll(h: H, as: A*): H =
+    as.foldLeft(h)((hh, a) => insert(a, hh))
+
+  @annotation.tailrec
+  final def readAllDecreasing(h: H, as: List[A] = Nil): List[A] =
+    if(isEmpty(h)) as else readAllDecreasing(deleteMin(h), findMin(h) :: as)
+
+  lazy val genHeap: Gen[H] = for{
+    len <- arbitrary[A].suchThat(i => i >= 0 && i < 100)
+    i <- arbitrary[A]
+    h <- oneOf(value(empty), genHeap)
+  } yield insert(i, h)
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
